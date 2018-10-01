6a8f34f0b099f142539d09c02ed786fa9164281e
James McNeil <jmcneil@equalexperts.com>
2018-10-01 21:52:30 +0100

    Strips supplied address down to domain

--- page/page.go
+++ page/page.go
@@ -3,27 +3,33 @@ package page
 import "regexp"
 
 type Page struct {
-    domain  string
-    html    string
-    links   []string
+	domain string
+	html   string
+	links  []string
 }
 
 func parseLinks(domain string, html string) []string {
-    escapedDomain := regexp.QuoteMeta(domain)
-    regex := regexp.MustCompile(`<a[ ]+href=\".*` + escapedDomain + `(/.*)\".*</a>`)
-    matches := regex.FindAllStringSubmatch(html, -1)
-    links := make([]string, len(matches))
-    for index, match := range matches {
-        links[index] = match[1]
-    }
+	escapedDomain := regexp.QuoteMeta(domain)
+	regex := regexp.MustCompile(`<a[ ]+href=\".*` + escapedDomain + `(/.*)\".*</a>`)
+	matches := regex.FindAllStringSubmatch(html, -1)
+	links := make([]string, len(matches))
+	for index, match := range matches {
+		links[index] = match[1]
+	}
 
-    return links
+	return links
 }
 
-func NewPage(domain string, html string) *Page {
-    page := new(Page)
-    page.html = html
-    page.domain = domain
-    page.links = parseLinks(domain, html)
-    return page
+func stripURL(address string) string {
+	regex := regexp.MustCompile(`(?:http://|https://|)(?:.+\.|)(.+\..+)`)
+	match := regex.FindStringSubmatch(address)
+	return match[1]
+}
+
+func NewPage(address string, html string) *Page {
+	page := new(Page)
+	page.html = html
+	page.domain = stripURL(address)
+	page.links = parseLinks(page.domain, html)
+	return page
 }
--- page/page_test.go
+++ page/page_test.go
@@ -1,27 +1,56 @@
 package page
 
 import (
-    "testing"
+	"testing"
 )
 
-func TestPageHasLinks(t *testing.T) {
-    htmlString := `<html>
-    <body>
-    <a href="https://www.monzo.com/accounts"</a>
-    <a href="monzo.com/support"</a>
-    </body>
-    </html>`
-    page := NewPage("monzo.com", htmlString)
+func TestPage(t *testing.T) {
+	const htmlString = `
+        <a href="https://www.monzo.com/accounts"</a>
+        <a href="monzo.com/support"</a>
+		<a href="http://monzo.com/advice"</a>
+		<a href="http://www.monzo.com/banking/loans"</a>
+	`
 
-    expected := map[string]bool{"/accounts": true, "/support": true}
+	t.Run("New Page extracts links from html string", func(t *testing.T) {
 
-    for _, link := range page.links {
-        if expected[link] != true {
-            t.Errorf("'%s' was not present in links", link)
-        }
-    }
+		page := NewPage("monzo.com", htmlString)
 
-    if len(page.links) != 2 {
-        t.Errorf("expected %d links got %d ", 2, len(page.links))
-    }
+		expected := map[string]bool{
+			"/accounts":      true,
+			"/support":       true,
+			"/advice":        true,
+			"/banking/loans": true}
+
+		for _, link := range page.links {
+			if expected[link] != true {
+				t.Errorf("'%s' was not present in links", link)
+			}
+		}
+
+		if len(page.links) != len(expected) {
+			t.Errorf("expected %d links got %d ", 2, len(page.links))
+		}
+	})
+
+	t.Run("Passing a URL matches links where only domain is specified", func(t *testing.T) {
+		page := NewPage("https://www.monzo.com", htmlString)
+
+		expected := map[string]bool{
+			"/accounts":      true,
+			"/support":       true,
+			"/advice":        true,
+			"/banking/loans": true}
+
+		for _, link := range page.links {
+			if expected[link] != true {
+				t.Errorf("'%s' was not present in links", link)
+			}
+		}
+
+		if len(page.links) != len(expected) {
+			t.Errorf("expected %d links got %d ", 2, len(page.links))
+		}
+	})
+
 }

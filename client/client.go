af1a7f3d6129cf554025fbd2598477ae8a37a38d
James McNeil <jmcneil@equalexperts.com>
2018-10-02 09:09:07 +0100

    Adds http client with timeout

---
+++ client/client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+    "io/ioutil"
+    "log"
+    "net/http"
+    "time"
+)
+
+type Client struct {
+    client *http.Client
+}
+
+func (httpClient *Client) Get(url string) (string, error) {
+
+    resp, err := httpClient.client.Get(url)
+    if err != nil {
+        log.Printf("Unable to Get from %s: %s\n", url, err)
+        return "", err
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Printf("Unable to read body from %s: %s\n", url, err)
+        return "", err
+    }
+    return string(body), nil
+}
+
+func NewClient() *Client {
+    httpClient := new(Client)
+    httpClient.client = &http.Client{
+        Timeout: time.Second * 5,
+    }
+
+    return httpClient
+}

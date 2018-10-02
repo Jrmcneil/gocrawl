761371c5c20652c7d5272bb19cb1eb43d1a8e228
James McNeil <jmcneil@equalexperts.com>
2018-10-02 15:47:28 +0100

    Refactors page methods

--- page/page.go
+++ page/page.go
@@ -8,16 +8,26 @@ type Page struct {
 }
 
 func (page *Page) ParseLinks(html string) {
-	domain := stripURL(page.address)
-	escapedDomain := regexp.QuoteMeta(domain)
-	regex := regexp.MustCompile(`<a[ ]+href=\".*` + escapedDomain + `(/.*)\".*</a>`)
-	matches := regex.FindAllStringSubmatch(html, -1)
-	links := make([]string, len(matches))
-	for index, match := range matches {
-		links[index] = match[1]
-	}
+    matches := findMatches(page.address, html)
+    page.links = setLinks(matches)
+}
 
-	page.links = links
+func setLinks(matches [][]string) []string {
+    links := make([]string, len(matches))
+    for index, match := range matches {
+        links[index] = match[1]
+    }
+    return links
+}
+
+func findMatches(address string, html string) [][]string {
+    regex := regexp.MustCompile(`<a[ ]+href=\".*` + escapedDomain(address) + `(/.*)\".*</a>`)
+    return regex.FindAllStringSubmatch(html, -1)
+}
+
+func escapedDomain(address string) string {
+    domain := stripURL(address)
+    return regexp.QuoteMeta(domain)
 }
 
 func stripURL(address string) string {

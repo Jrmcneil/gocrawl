59166df672f0829f2b519ab90bd96793aee44df9
James McNeil <jmcneil@equalexperts.com>
2018-10-03 21:54:11 +0100

    Adds sitemap builder
    Note: the recursive design for the tree printer was taken from Stack
    Overflow

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -2,8 +2,38 @@ package sitemap
 
 import (
     "gocrawl/job"
+    "strings"
 )
 
 type Sitemap struct {
-    root job.Job
+    Result chan string
+}
+
+func (sitemap *Sitemap) Build(root job.Job) {
+    go func() {
+        var sb strings.Builder
+        var indentSb strings.Builder
+        indentSb.WriteString("")
+        buildMap(root, sb, indentSb, true)
+        sitemap.Result <- sb.String()
+    } ()
+}
+
+func buildMap(node job.Job, sb strings.Builder, indentSb strings.Builder, last bool) {
+    sb.WriteString(indentSb.String())
+
+    if last {
+        sb.WriteString("\\-")
+        indentSb.WriteString("  ")
+    } else {
+        sb.WriteString("|-")
+        indentSb.WriteString("| ")
+    }
+    sb.WriteString(node.Address() + "\n")
+
+    <- node.Ready()
+
+    for i := 0; i < len(node.Links()); i++ {
+        buildMap(node.Links()[i], sb, indentSb, i == len(node.Links()) - 1)
+    }
 }
